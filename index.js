5a5a34c238316b8f27c6efcb06ed08fbf59ebff0
Rendon Elsesser <rendonelsesser@gmail.com>
2018-08-23 18:34:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
 
-function holidayCountdown(holiday,name) {
+function holidayCountdown(holiday, name) {
   return `Its ${days} days until ${holiday}!`
 }
